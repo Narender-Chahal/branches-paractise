ea6060fb8bc3b3894bf877b645704ba43a724e51
Mohit-Godara-157 <mohitgodara646@gmail.com>
2023-04-26 09:54:02 +0530

    branch

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Head from "./components/Head";
+import MyNav from "./components/MyNav";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <Head />
       <NavBar />
+      <MyNav />
+      <About/>
     </>
   );
 }
-
-export default App;
