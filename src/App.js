3df68c55ff72c4cc6b175fb7d13c0809766fed2a
Kuldeep-Kumar-150 <swamikuldeep654@gmail.com>
2023-04-26 09:54:00 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./components/NavBar";
 import Head from "./components/Head";
-import MyNav from './components/MyNav';
-import NavBar from './components/NavBar';
+import MyNav from "./components/MyNav";
+import MyFooter from "./components/MyFooter";
 
 function App() {
   return (
     <>
       <Head />
+      <NavBar />
       <MyNav />
-      <NavBar /></>
-  )
+      <MyFooter/>
+    </>
+  );
 }
-export default App;
---
+++ src/components/MyFooter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MyFooter = () => {
+    return (
+        <div>
+            <h6>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita numquam
+                ex repudiandae, eligendi explicabo deserunt quidem dolorum praesentium
+                accusamus eius totam hic minus velit architecto modi ut consequatur itaque
+                voluptates.
+            </h6>
+        </div>
+    )
+}
+
+export default MyFooter
