6caf49d6c5d6aa4d5c1b2e5c01420ae76e19f234
Mohit-Godara-157 <mohitgodara646@gmail.com>
2023-04-26 10:00:59 +0530

    addas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import Head from "./components/Head";
 import MyNav from "./components/MyNav";
+import About from "./components/About";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Head />
       <NavBar />
       <MyNav />
+      <About />
     </>
   );
 }
