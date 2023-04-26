6ca54bd989852f19b55e48ad4b20656207860293
Kuldeep-Kumar-150 <swamikuldeep654@gmail.com>
2023-04-26 09:48:47 +0530

    addds

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
-<<<<<<< HEAD
 import logo from "./logo.svg";
 import "./App.css";
 import Head from "./components/Head";
+import MyNav from './components/MyNav';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
       <Head />
-    </>
-=======
-import logo from './logo.svg';
-import './App.css';
-<<<<<<< HEAD
-import MyNav from './components/MyNav';
-
-function App() {
-  return (
-    <>
-    <MyNav/>
-    </>
-=======
-import NavBar from './components/NavBar';
-
-function App() {
-  return (
-   <>
-   <NavBar/>
-   </>
->>>>>>> 0a0df2880dbe4ac682965d59cbcf444adb93ddf2
-  );
+      <MyNav />
+      <NavBar /></>
+  )
 }
-
 export default App;
