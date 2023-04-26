0166c8049cb8c03be4fb7725d82762f98e0bf044
Kuldeep-Kumar-150 <swamikuldeep654@gmail.com>
2023-04-26 09:37:52 +0530

    commit

---
+++ src/components/MyNav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MyNav = () => {
+    return (
+        <div>
+            <h1>BRNCH-LEARNING</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum quis
+                libero sint eum adipisci, quae quibusdam iure tempore dicta aut, dolore
+                sed molestias quaerat facilis deleniti fuga ad animi cupiditate!</p>
+        </div>
+    )
+}
+
+export default MyNav
